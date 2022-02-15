ddba2afd919991a3f05401e03ce402ca24e25d15
adityarfw <aditya.rajkumar@wisetail.com>
2022-02-15 12:46:06 -0500

    Updated check for password match, Removed additional spaces

--- script.js
+++ script.js
@@ -4,59 +4,56 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-
 // Show Input Success message
-function showSuccess(input){
-    const formControl = input.parentElement;
-    formControl.className = 'form-control success';
-    
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
 }
 
 // Show Input Error messsage
-function showError(input, message){
-    const formControl = input.parentElement;
-    formControl.className = 'form-control error';
-    const small = formControl.querySelector('small');
-    small.innerText = message;
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
 }
 
 // Check Email
 function isValidEmail(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
 
-
-
 // Event Liseteners
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
 
-    if (username.value === ''){
-        showError(username, 'Username is required');
-    } else {
-        showSuccess(username);
-    }
+  if (username.value === '') {
+    showError(username, 'Username is required');
+  } else {
+    showSuccess(username);
+  }
 
-    if (email.value === ''){
-        showError(email, 'Email is required');
-    }
-    else if(!isValidEmail(email.value)){
-        showError(email, 'Email is not Valid');
-    } else {
-        showSuccess(email);
-    }
+  if (email.value === '') {
+    showError(email, 'Email is required');
+  } else if (!isValidEmail(email.value)) {
+    showError(email, 'Email is not Valid');
+  } else {
+    showSuccess(email);
+  }
 
-    if (password.value === ''){
-        showError(password, 'Password is required');
-    } else {
-        showSuccess(password);
-    }
+  if (password.value === '') {
+    showError(password, 'Password is required');
+  } else {
+    showSuccess(password);
+  }
 
-    if (password2.value === ''){
-        showError(password2, 'Password is required Again!');
-    } else {
-        showSuccess(password2);
-    }
-
+  if (password2.value === '') {
+    showError(password2, 'Password is required Again!');
+  } else if (password2.value !== password.value) {
+    showError(password2, 'Passwords do not match!');
+  } else {
+    showSuccess(password2);
+  }
 });
